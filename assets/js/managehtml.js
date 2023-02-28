a572f306f490964d6d425812521b966b64da0716
patrita63 <patrita63@gmail.com>
2023-02-28 22:57:59 +0100

    it-IT

--- assets/js/managehtml.js
+++ assets/js/managehtml.js
@@ -7,15 +7,6 @@ function includeHTML(language) {
   var z, i, elmnt, file, xhttp;
 
   console.log('Inside includeHTML! - language:' + language);
-  // To Avoid CORS ERROR WHEN TEST LOCALLY
-  // debugger;
-  let filePath = "";
-  if (language = "en-US") {
-    filePath = urlFileEn + file;
-  } else if (language = "it-IT") {
-    filePath = urlFileIt + file;
-  }
-  console.log('includeHTML filePath: ' + filePath);
 
   /* Loop through a collection of all HTML elements: */
   z = document.getElementsByTagName("*");
@@ -23,6 +14,15 @@ function includeHTML(language) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("w3-include-html");
+    // To Avoid CORS ERROR WHEN TEST LOCALLY
+    // debugger;
+    let filePath = "";
+    if (language = "en-US") {
+      filePath = urlFileEn + file;
+    } else if (language = "it-IT") {
+      filePath = urlFileIt + file;
+    }
+    console.log('includeHTML filePath: ' + filePath);
     if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
