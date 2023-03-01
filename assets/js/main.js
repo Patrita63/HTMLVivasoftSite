bceb87c198bb80f97619974c5aa1130c710d3f4d
patrita63 <patrita63@gmail.com>
2023-03-01 10:44:49 +0100

    <!-- Cookie Message --> solo su index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -290,7 +290,7 @@ jQuery(function ($) {
     // Switch Btn
 	// $('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
 
-    $('body').append("<div id='cookie' class='cookie-box'><label id='cookie' class='cookie'><span>Utilizziamo i cookies per garantire la funzionalità del sito e per tenere conto delle vostre scelte di navigazione.<br>Continuando la navigazione, accetti di utilizzare i cookies. Maggiori Informazioni ...</span><input type='button' value='Accetta' onclick='hideCookieMessage()'></label></div>");
+    $('body').append("<div id='cookie' class='cookie-box'><label id='cookie'><span>Utilizziamo i cookies per garantire la funzionalità del sito e per tenere conto delle vostre scelte di navigazione.<br>Continuando la navigazione, accetti di utilizzare i cookies.<br>Consulta la nostra <a href='privacy.html'>Informativa sulla privacy</a>.</span></label><div class='slider-btn text-center'><input type='button' class='box-btn-gray' value='Rifiuta' onclick='hideCookieMessage()'>   <input type='button' class='box-btn-selected' value='Accetta tutto' onclick='hideCookieMessage()'></div></div>");
 
 }(jQuery));
 
